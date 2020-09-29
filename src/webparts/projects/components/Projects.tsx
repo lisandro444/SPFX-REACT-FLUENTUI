1c4356f9cf10d353c3b3ccfa00ada9d5f8fbf81a
Lisandro Rossi <lisandrorossi444@gmail.com>
2020-09-29 11:26:56 -0300

    #get all documentByProjects

--- src/webparts/projects/components/Projects.tsx
+++ src/webparts/projects/components/Projects.tsx
@@ -50,6 +50,10 @@ export default class Projects extends React.Component<IProjectsProps, IProjectsP
       });
   });
 
+  this._pnpService.GetAllDocumentsFromProject("https://fonplataorg.sharepoint.com/sites/ITS_ARG_Contratos_123").then(docs => {
+    console.log(docs);
+  });
+
   const queryText = "'sharepoint'&selectproperties='Path,Url,Title,Author'";
   const queryText2 = "'https://fonplataorg.sharepoint.com/sites/ITS_ARG_Seguimiento'&selectproperties='TipoDocumentoURL'";
   this._pnpService.getSearchResults(queryText2).then(result => {
--- src/webparts/projects/services/PnPService.ts
+++ src/webparts/projects/services/PnPService.ts
@@ -5,15 +5,16 @@ import "@pnp/sp/items";
 import { BaseWebPartContext } from "@microsoft/sp-webpart-base";
 import { Web } from '@pnp/sp/webs';
 import { ICamlQuery } from '@pnp/sp/lists';
-import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';  
+import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
 // import { IWebPartContext } from '@microsoft/sp-webpart-base';  
-import { ISPSearchResult } from '../components/ISPSearchResult';  
-import { ISearchResults, ICells, ICellValue, ISearchResponse } from './ISearchService';  
-import { escape } from '@microsoft/sp-lodash-subset'; 
+import { ISPSearchResult } from '../components/ISPSearchResult';
+import { ISearchResults, ICells, ICellValue, ISearchResponse } from './ISearchService';
+import { escape } from '@microsoft/sp-lodash-subset';
 
 export class PnPService {
     private _context;
     private _siteUrl;
+    private docsLibraries: any = ["Analisis","Adquisiciones", "Contrato", "Financieros"];
     constructor(context: BaseWebPartContext) {
         this._context = context;
         this._siteUrl = context.pageContext.web.absoluteUrl;
@@ -78,96 +79,116 @@ export class PnPService {
         }
     }
 
+    // Get Documents
+    public async GetAllDocumentsFromProject(url): Promise<any[]> {
+        const resultsDocs: any[] = [] ;
+        try {
+            let initialweb = Web(url);
+            initialweb.lists.get().then(data => {
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].BaseType == "1" && this.docsLibraries.indexOf(data[i].Title) > -1) {
+                        let docfromList = initialweb.lists.getByTitle(data[i].Title).items.getAll();
+                        resultsDocs.push(docfromList);
+                    }
+                }
+                return resultsDocs;
+            }).catch(data => { console.log(data); });
+        } catch (error) {
+            console.log("GetAllDocumentsFromProject: " + error);
+            return error;
+        }
+    }
+
     // Search Logic
 
-    public async getSearchResults(query: string): Promise<ISPSearchResult[]> {  
-          
-        let url: string = this._context.pageContext.web.absoluteUrl + "/_api/search/query?querytext=" + query;  
-         console.log(url); 
-        return new Promise<ISPSearchResult[]>((resolve, reject) => {  
+    public async getSearchResults(query: string): Promise<ISPSearchResult[]> {
+
+        let url: string = this._context.pageContext.web.absoluteUrl + "/_api/search/query?querytext=" + query;
+        console.log(url);
+        return new Promise<ISPSearchResult[]>((resolve, reject) => {
             // Do an Ajax call to receive the search results  
-            this._getSearchData(url).then((res: ISearchResults) => {  
-                let searchResp: ISPSearchResult[] = [];  
-  
+            this._getSearchData(url).then((res: ISearchResults) => {
+                let searchResp: ISPSearchResult[] = [];
+
                 // Check if there was an error  
-                if (typeof res["odata.error"] !== "undefined") {  
-                    if (typeof res["odata.error"]["message"] !== "undefined") {  
-                        Promise.reject(res["odata.error"]["message"].value);  
-                        return;  
-                    }  
-                }  
-  
-                if (!this._isNull(res)) {  
-                    const fields: string = "Title,Path,Description";  
-  
+                if (typeof res["odata.error"] !== "undefined") {
+                    if (typeof res["odata.error"]["message"] !== "undefined") {
+                        Promise.reject(res["odata.error"]["message"].value);
+                        return;
+                    }
+                }
+
+                if (!this._isNull(res)) {
+                    const fields: string = "Title,Path,Description";
+
                     // Retrieve all the table rows  
-                    if (typeof res.PrimaryQueryResult.RelevantResults.Table !== 'undefined') {  
-                        if (typeof res.PrimaryQueryResult.RelevantResults.Table.Rows !== 'undefined') {  
-                            searchResp = this._setSearchResults(res.PrimaryQueryResult.RelevantResults.Table.Rows, fields);  
-                        }  
-                    }  
-                }  
-  
+                    if (typeof res.PrimaryQueryResult.RelevantResults.Table !== 'undefined') {
+                        if (typeof res.PrimaryQueryResult.RelevantResults.Table.Rows !== 'undefined') {
+                            searchResp = this._setSearchResults(res.PrimaryQueryResult.RelevantResults.Table.Rows, fields);
+                        }
+                    }
+                }
+
                 // Return the retrieved result set  
-                resolve(searchResp);  
-            });  
-        });  
-    }  
-  
-     /** 
-     * Retrieve the results from the search API 
-     * 
-     * @param url 
-     */  
-    private _getSearchData(url: string): Promise<ISearchResults> {  
-        return this._context.spHttpClient.get(url, SPHttpClient.configurations.v1, {  
-            headers: {  
-                'odata-version': '3.0'  
-            }  
-        }).then((res: SPHttpClientResponse) => {  
-            return res.json();  
-        }).catch(error => {  
-            return Promise.reject(JSON.stringify(error));  
-        });  
-    }  
-  
+                resolve(searchResp);
+            });
+        });
+    }
+
+    /** 
+    * Retrieve the results from the search API 
+    * 
+    * @param url 
+    */
+    private _getSearchData(url: string): Promise<ISearchResults> {
+        return this._context.spHttpClient.get(url, SPHttpClient.configurations.v1, {
+            headers: {
+                'odata-version': '3.0'
+            }
+        }).then((res: SPHttpClientResponse) => {
+            return res.json();
+        }).catch(error => {
+            return Promise.reject(JSON.stringify(error));
+        });
+    }
+
     /** 
      * Set the current set of search results 
      * 
      * @param crntResults 
      * @param fields 
-     */  
-    private _setSearchResults(crntResults: ICells[], fields: string): any[] {  
-        const temp: any[] = [];  
-  
-        if (crntResults.length > 0) {  
-            const flds: string[] = fields.toLowerCase().split(',');  
-  
-            crntResults.forEach((result) => {  
+     */
+    private _setSearchResults(crntResults: ICells[], fields: string): any[] {
+        const temp: any[] = [];
+
+        if (crntResults.length > 0) {
+            const flds: string[] = fields.toLowerCase().split(',');
+
+            crntResults.forEach((result) => {
                 // Create a temp value  
-                var val: Object = {}  
-  
-                result.Cells.forEach((cell: ICellValue) => {  
-                    if (flds.indexOf(cell.Key.toLowerCase()) !== -1) {  
+                var val: Object = {}
+
+                result.Cells.forEach((cell: ICellValue) => {
+                    if (flds.indexOf(cell.Key.toLowerCase()) !== -1) {
                         // Add key and value to temp value  
-                        val[cell.Key] = cell.Value;  
-                    }  
-                });  
-  
+                        val[cell.Key] = cell.Value;
+                    }
+                });
+
                 // Push this to the temp array  
-                temp.push(val);  
-            });  
-        }  
-  
-        return temp;  
-    }  
-  
+                temp.push(val);
+            });
+        }
+
+        return temp;
+    }
+
     /** 
      * Check if the value is null or undefined 
      * 
      * @param value 
-     */  
-    private _isNull(value: any): boolean {  
-        return value === null || typeof value === "undefined";  
-    }  
+     */
+    private _isNull(value: any): boolean {
+        return value === null || typeof value === "undefined";
+    }
 } 
